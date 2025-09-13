ec1955ab6d8bf41faf179030cc920a82e5b6c63b
Chandradev819 <chandradev819@gmail.com>
2025-09-13 17:18:45 +0530

    Validation Code.

---
+++ app/Shared/Emp.cs
@@ -0,0 +1,27 @@
+﻿namespace InvoiceApp.Shared
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.AspNetCore.Components.Forms; // Needed for [ValidatableType]
+
+    [ValidatableType]
+    public class Emp
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        public string? LastName { get; set; }
+
+        public Address Address { get; set; } = new();
+    }
+
+    public class Address
+    {
+        [Required(ErrorMessage = "Street is required.")]
+        public string? Street { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        public string? City { get; set; }
+    }
+
+}
