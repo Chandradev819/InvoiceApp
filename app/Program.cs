3c9ce9bf28fe033e5286357160aa84e1e81c84d3
Chandradev819 <chandradev819@gmail.com>
2025-09-14 18:06:42 +0530

    Revert "Login Code"
    This reverts commit 2a7b35a1bc75bd5ca30bc4c2b15fdbd9e351c795.

--- app/Program.cs
+++ app/Program.cs
@@ -1,13 +1,11 @@
-using AzureStaticWebApps.Blazor.Authentication;
-using InvoiceApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using InvoiceApp;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
-    .AddStaticWebAppsAuthentication();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddValidation();
 await builder.Build().RunAsync();
